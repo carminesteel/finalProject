cb9583c9769124f4c3780b592c5243c63c234909
carminesteel <mymyhongz@naver.com>
2020-08-03 13:08:34 +0900

    ???

--- final/src/main/java/com/example/hansub_project/service/member/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.example.hansub_project.service.member;
-
-import com.example.domain.UsersVO;
-
-public interface MemberService {
-
-	public void authentication(UsersVO vo); // 회원 인증관련 메소드
-
-}
--- final/src/main/java/com/example/mapper/CartMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-import com.example.domain.CartVO;
-
-public interface CartMapper {
-	public void orders();
-	public void cartinsert(CartVO cvo);
-}
